f029620f4e8efc1e551515936fa066a11b852602
Precious Aguluka <agulukachioma@gmail.com>
2025-09-29 19:54:15 +0100

    taking care of the line items

--- app/utils/payload.py
+++ app/utils/payload.py
@@ -3,17 +3,17 @@ import jwt
 from fastapi import HTTPException, status
 
 def decode_token(token):
-   try:
-       payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
-       return payload
+    try:
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        return payload
   
-   except jwt.ExpiredSignatureError:
-       raise HTTPException(
-           status_code=status.HTTP_401_UNAUTHORIZED,
-           detail="Token has expired. Please log in again.",
-       )
-   except jwt.InvalidTokenError:
-       raise HTTPException(
-           status_code=status.HTTP_401_UNAUTHORIZED,
-           detail="Invalid token. Please log in again.",
-       )
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired. Please log in again.",
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token. Please log in again.",
+        )
