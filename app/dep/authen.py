fdd3e5ea9b35fe6ef3012eeb8b63c980406660fe
Precious Aguluka <agulukachioma@gmail.com>
2025-09-29 19:30:14 +0100

    added authentication routes for user,student, admin and superadmin
    define utilities to decode jwt token
    
    define authentication utilities to check if user

---
+++ app/dep/authen.py
@@ -0,0 +1,85 @@
+from fastapi.security import OAuth2PasswordBearer
+from app.database_setup import get_db
+from fastapi import Depends, HTTPException, status
+from ..utils.authen import is_blacklisted
+from ..utils.payload import decode_token
+
+# Define OAuth2 scheme
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user/login")
+
+async def user_auth(token=Depends(oauth2_scheme), session=Depends(get_db)):
+   """
+   user authentication dependency.
+   """
+   try:
+       await is_blacklisted(token, session)
+       return token 
+   except Exception as e:
+       raise HTTPException(
+           status_code=status.HTTP_401_UNAUTHORIZED,
+           detail=str(e)
+       )
+   
+def student_auth(token=Depends(user_auth)):
+    """
+    Dependency to ensure the user is an admin or superadmin.
+    """
+    try:
+        payload = decode_token(token)
+        role = payload.get("role")
+        if role not in ["student", "teacher", "admin", "superadmin"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="At least student privileges required."
+            )
+        return token
+    except HTTPException as http_exc:
+       raise http_exc 
+   
+def teacher_auth(token=Depends(student_auth)):
+    """
+    Dependency to ensure the user is an admin or superadmin.
+    """
+    try:
+        payload = decode_token(token)
+        role = payload.get("role")
+        if role not in ["teacher", "admin", "superadmin"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="At least teacher privileges required."
+            )
+        return token
+    except HTTPException as http_exc:
+       raise http_exc   
+   
+def admin_auth(token=Depends(teacher_auth)):
+    """
+    Dependency to ensure the user is an admin or superadmin.
+    """
+    try:
+        payload = decode_token(token)
+        role = payload.get("role")
+        if role not in ["admin", "superadmin"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="At least admin privileges required."
+            )
+        return token
+    except HTTPException as http_exc:
+       raise http_exc
+   
+def superadmin_auth(token=Depends(admin_auth)):
+   """
+   Dependency to ensure the user is a superadmin.
+   """
+   try:
+       payload = decode_token(token)
+       role = payload.get("role")
+       if role != "superadmin":
+           raise HTTPException(
+               status_code=status.HTTP_403_FORBIDDEN,
+               detail="Superadmin privileges required."
+           )
+       return token
+   except HTTPException as http_exc:
+       raise http_exc
---
+++ app/utils/authen.py
@@ -0,0 +1,28 @@
+from ..model.lms_tables import BlackList, Users
+from sqlmodel import select
+from fastapi import HTTPException, status
+from sqlalchemy import and_
+from .payload import decode_token
+
+
+
+async def is_blacklisted(token, session) -> bool:
+    try:
+        payload = decode_token(token)
+        email = payload.get("email")
+        id = payload.get("id")
+        statement = select(Users).where(and_(Users.email == email, Users.id == id))
+        result = await session.execute(statement)
+        user = result.scalars().first()
+        if not user:
+           raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        if not user.is_active:
+           raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User account is inactive")
+        statement = select(BlackList).where(BlackList.black_token == token)
+        stat_result = await session.execute(statement)
+        result = stat_result.scalars().first()
+        if result:
+           raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token has been blacklisted. Please log in again.")
+        return False
+    except HTTPException as http_exc:
+        raise http_exc
