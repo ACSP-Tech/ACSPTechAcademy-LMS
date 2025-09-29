7fce973802f7ba0d05cf5d1eda66a4634636bad2
Precious Aguluka <agulukachioma@gmail.com>
2025-09-29 18:21:08 +0100

    defining initial model

--- app/model/lms_tables.py
+++ app/model/lms_tables.py
@@ -23,7 +23,7 @@ class Users(SQLModel, table=True):
         sa_column=Column(String, nullable=False))
     gender: str = Field(
         sa_column=Column(String, nullable=False))
-    course:str = Field(default="na",
+    course: str = Field(default="Na",
         sa_column=Column(String, nullable=False, index=True))
     role: str = Field(
         sa_column=Column(String, nullable=False, index=True))
@@ -36,8 +36,117 @@ class Users(SQLModel, table=True):
     current_stage: int = Field(default= 0, sa_column=Column(Integer, nullable=False, index=True))
     sub_deny_count:int = Field(default= 0, sa_column=Column(Integer, nullable=False, index=True))
 
+    #defining relationships
+    tasks: List["Task"] = Relationship(back_populates="users")
+    blacklists: List["BlackList"] = Relationship(back_populates="users")
+    Usersubscriptions: List["UserSubscription"] = Relationship(back_populates="users")
+    subscriptions: List["Subscription"] = Relationship(back_populates="users")
+    classrooms: List["ClassRoom"] = Relationship(back_populates="users")
+    studenttasks: List["StudentTask"] = Relationship(back_populates="users")
+
+    #validators
     @field_validator("course", "role", mode = "before")
     def normalize_fields(cls, value):
         if isinstance(value, str):
-            return value.strip().lower()
+            return value.strip().titlecase()
         return value
+
+class BlackList(SQLModel, table=True):
+    black_token: str = Field(primary_key=True)
+    created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False))
+    #foreign keys
+    user_id: str = Field(foreign_key="user.id")
+
+    #defining relationships
+    users: Optional["Users"] = Relationship(back_populates="blacklists")
+
+class Task(SQLModel, table=True):
+    task_id: str = Field(
+    default_factory=lambda: str(uuid.uuid4()),
+    sa_column=Column(String(36), primary_key=True, nullable=False))
+    task_name: str = Field(sa_column=Column(String, nullable=False, index=True))
+    stage: int = Field(sa_column=Column(Integer, nullable=False, index=True)) 
+    task_details: str = Field(sa_column=Column(String, nullable=False, index=True))
+    due_date: datetime = Field(sa_column=Column(DateTime(timezone=True), nullable=False))
+    version: int = Field(default= 0, sa_column=Column(Integer, nullable=False, index=True))
+    created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False))
+    updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False))
+    status: str = Field(default="Pending", sa_column=Column(String, nullable=False, index=True))
+    course: str = Field(default="Data Analysis", sa_column=Column(String, nullable=False, index=True))
+    level: str = Field(default="Beginner", sa_column=Column(String, nullable=False, index=True))
+    #foreign key
+    user_id: str = Field(foreign_key="user.id")
+    #relationships
+    users: Optional["Users"] = Relationship(back_populates="tasks")
+    #define relationships
+    classrooms: List["ClassRoom"] = Relationship(back_populates="task")
+    studenttasks: List["StudentTask"] = Relationship(back_populates="task")
+    #field_validator
+    @field_validator("course", "role", mode = "before")
+    def normalize_fields(cls, value):
+        if isinstance(value, str):
+            return value.strip().titlecase()
+        return value
+
+class StudentTask(SQLModel, table=True):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), sa_column=Column(String(36), primary_key=True, nullable=False))
+    email: str = Field(sa_column=Column(String, nullable=False, index=True))
+    status: str = Field(default="Submitted", sa_column=Column(String, nullable=False, index=True))
+    submission_link: str = Field(sa_column=Column(String, nullable=False, index=True))
+    score: int = Field(sa_column=Column(Integer, nullable=False, index=True))
+    created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False))
+    updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False))
+    #foreign key
+    user_id: str = Field(foreign_key="user.id")
+    task_id: str = Field(foreign_key="task.task_id")
+    #relationships
+    users: Optional["Users"] = Relationship(back_populates="studenttasks")
+    task: Optional["Task"] = Relationship(back_populates="studenttasks")
+
+
+class ClassRoom(SQLModel, table=True):
+    class_id: str = Field(default_factory=lambda: str(uuid.uuid4()), sa_column=Column(String(36), primary_key=True, nullable=False))
+    video_url: str = Field(sa_column=Column(String, nullable=False, index=True))
+    video_public_id: str = Field(sa_column=Column(String, nullable=False, index=True))
+    view_count: int = Field(sa_column=Column(Integer, nullable=False, index=True))
+    duration: int = Field(sa_column=Column(Integer, nullable=False, index=True))
+    thumbnail_url: str = Field(sa_column=Column(String, nullable=False, index=True))
+    folder: str = Field(sa_column=Column(String, nullable=False, index=True))
+    created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False))
+    updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False))
+    #foreign key
+    user_id: str = Field(foreign_key="user.id")
+    task_id: str = Field(foreign_key="task.task_id")
+    #relationships 
+    users: Optional["Users"] = Relationship(back_populates="classrooms") #one to many
+    task: Optional["Task"] = Relationship(back_populates="classrooms") #one to one
+
+class Subscription(SQLModel, table=True):
+    sub_id: str = Field(default_factory=lambda: str(uuid.uuid4()), sa_column=Column(String(36), primary_key=True, nullable=False))
+    sub_details: str = Field(sa_column=Column(String, nullable=False))
+    acc_details: str = Field(sa_column=Column(String, nullable=False))
+    status: str = Field(default="Active", sa_column=Column(String, nullable=False, index=True))
+    version: int = Field(sa_column=Column(Integer, nullable=False, index=True))
+    course: str = Field(sa_column=Column(String, nullable=False, index=True))
+    #foreign key
+    user_id: str = Field(foreign_key="user.id")
+    #relationships 
+    users: Optional["Users"] = Relationship(back_populates="subscriptions")
+    #defining relationships
+    usersubscriptions: List["UserSubscription"] = Relationship(back_populates="subscription")
+
+class UserSubscription(SQLModel, table=True):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), sa_column=Column(String(36), primary_key=True, nullable=False))
+    status: str = Field(default="Pending", sa_column=Column(String, nullable=False, index=True))
+    payment_url: str = Field(sa_column=Column(String, nullable=False, index=True))
+    payment_public_id: str = Field(sa_column=Column(String, nullable=False, index=True))
+    total_pages: int = Field(default= 0, sa_column=Column(Integer, nullable=False, index=True))
+    filename: str = Field(sa_column=Column(String, nullable=False, index=True))
+    filetype: str = Field(sa_column=Column(String, nullable=False, index=True))
+    sub_status: str = Field(default="Inactive", sa_column=Column(String, nullable=False, index=True))
+    #foreign key
+    user_id: str = Field(foreign_key="user.id")
+    sub_id: str = Field(foreign_key="subscription.sub_id")
+    #relationships 
+    users: Optional["Users"] = Relationship(back_populates="usersubscriptions")
+    subscription: Optional["Subscription"] = Relationship(back_populates="usersubscriptions")
