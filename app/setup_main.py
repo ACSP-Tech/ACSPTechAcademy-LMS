91fa4dcad9a3085e5f4dbc50bdaba02d073a6200
Precious Aguluka <agulukachioma@gmail.com>
2025-10-01 01:53:54 +0100

    changing cron job to every 7 minutes

--- app/setup_main.py
+++ app/setup_main.py
@@ -9,13 +9,3 @@ def configure_cors(app):
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-#on stable production
-# def configure_cors(app): 
-#     app.add_middleware( 
-#         CORSMiddleware, 
-#         allow_origins=["https://yourfrontend.com"],  # Specific domains
-#         allow_credentials=True, 
-#         allow_methods=["GET", "POST", "PUT", "DELETE"],  # Specific methods
-#         allow_headers=["*"], 
-#     )
