6e97cfc25aa7d165ad492da47655268f5972754f
Precious Aguluka <agulukachioma@gmail.com>
2025-10-11 14:18:33 +0100

    debug wrong column name

--- app/crud/verify_otp.py
+++ app/crud/verify_otp.py
@@ -19,7 +19,7 @@ async def user_verify_otp(data, session):
                 detail="Invalid otp request, please request a new otp"
             )
         # Check if OTP matches
-        if otp_entry_check.otp != otp_code:
+        if otp_entry_check.otp_code != otp_code:
             otp_entry_check.attempts += 1
             await session.commit()
             await session.refresh(otp_entry_check)
